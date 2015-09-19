31b515f742cbaa348e4364d91ff6bbf431113b83
Jeff Millar <jeff.millar@sbcglobal.net>
2015-09-19 07:41:43 -0700

    second commit

---
+++ calculator.rb
@@ -0,0 +1,24 @@
+# This app will require 2 numbers from the user
+# The app will add, subtract, multiply or divide those 2 numbers
+
+puts "What is the first number?"
+first_num = gets.chomp
+
+puts "What is the second number?"
+second_num = gets.chomp
+
+puts "Would you like to a) add, b) subtract, c) multiply, or d) divide?"
+puts "Enter the letter:"
+op_variable = gets.chomp
+
+if op_variable == "a"
+  new_num = first_num.to_i + second_num.to_i
+elsif op_variable == "b"
+  new_num = first_num.to_i - second_num.to_i
+elsif op_variable == "c"
+  new_num = first_num.to_i * second_num.to_i
+else
+  new_num = first_num.to_f / second_num.to_f
+end
+
+puts "The result is #{new_num}"
